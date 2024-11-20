5841e22d4ed8109972838d54f02a7363efc7c13e
Alisa Steensen <alisakreif@gmail.com>
2024-11-20 10:35:55 -0600

    Pushing "11/20/24"

--- module-6/mysql_test.py
+++ module-6/mysql_test.py
@@ -1,13 +1,15 @@
+# Alisa Steensen
+# Module 6.2
+
 import mysql.connector
 from mysql.connector import errorcode
-
 
 config = {
     "user": "movies_user",
     "password": "popcorn",
-    "host" : "127.0.0.1",
+    "host": "127.0.0.1",
     "database": "movies",
-    "raise_on_warnings": True
+    "raise_on_warnings": True  
 }
 
 try:
